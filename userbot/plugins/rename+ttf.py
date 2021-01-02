f08cf05cc594ba6a6a50b3e86843cbceec023699
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-01-02 13:45:58 +0530

    Update and rename rename.py to rename+ttf.py

--- userbot/plugins/rename+ttf.py
+++ userbot/plugins/rename+ttf.py
@@ -2,7 +2,9 @@ import asyncio
 import os
 import time
 from datetime import datetime
-
+from telethon import events
+import asyncio
+from uniborg.util import admin_cmd
 from userbot.utils import admin_cmd, progress
 from userbot import CMD_HELP
 
@@ -73,6 +75,16 @@ async def _(event):
         await dcevent.edit(
            ".rename file.name as reply to a Telegram media/file"
         )
+        
+        
+@borg.on(admin_cmd(pattern="ttf ?(.*)"))
+async def get(event):
+    name = event.text[5:]
+    m = await event.get_reply_message()
+    with open(name, "w") as f:
+        f.write(m.message)
+    await event.delete()
+    await borg.send_file(event.chat_id,name,force_document=True)
 
 
 CMD_HELP.update(
