bcdddf50e19c56663732138f18bd5b041d78763a
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-01-02 13:38:55 +0530

    Update and rename selfdestruct.py to sd+decide.py

--- userbot/plugins/sd+decide.py
+++ userbot/plugins/sd+decide.py
@@ -6,7 +6,9 @@
 
 
 import time
-
+from telethon import events
+import requests
+from userbot.utils import admin_cmd
 from telethon.errors import rpcbaseerrors
 from userbot.utils import admin_cmd
 import importlib.util
@@ -30,3 +32,21 @@ async def selfdestruct(destroy):
         smsg = await destroy.client.send_message(destroy.chat_id, text)
         time.sleep(counter)
         await smsg.delete()
+        
+        
+@borg.on(admin_cmd("decide"))
+async def _(event):
+    if event.fwd_from:
+        return
+    message_id = event.message.id
+    if event.reply_to_msg_id:
+        message_id = event.reply_to_msg_id
+    r = requests.get("https://yesno.wtf/api").json()
+    await borg.send_message(
+        event.chat_id,
+        r["answer"],
+        reply_to=message_id,
+        file=r["image"]
+    )
+    await event.delete()
+
