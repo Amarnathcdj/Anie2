0a54e3ecc7d7491ed35d2e4dc3933e0a083672bf
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-01-02 13:42:32 +0530

    Update and rename shouts.py to scraout.py

--- userbot/plugins/scraout.py
+++ userbot/plugins/scraout.py
@@ -1,8 +1,7 @@
-"""Shouts a message in MEME way
-usage: .shout message
-originaly from : @corsicanu_bot
-"""
-
+#Credit goes to @Roseloverx
+import random
+import re
+from userbot.utils import admin_cmd
 import sys
 from telethon import events, functions
 from uniborg.util import admin_cmd
@@ -27,6 +26,31 @@ async def shout(args):
         result = "".join(result)
         msg = "\n" + result
         await args.edit("`"+msg+"`")
+
+        
+@borg.on(admin_cmd(pattern="scramble(\s+[\S\s]+|$)"))
+async def scramble_message(e):
+    reply_message = await e.get_reply_message()
+    text = e.pattern_match.group(1) or reply_message.text
+    words = re.split(r"\s", text)
+    scrambled = map(scramble_word, words)
+    text = ' '.join(scrambled)
+    await e.edit(text)
+
+
+def scramble_word(word):
+    if len(word) < 4:
+        return word
+
+    first_letter = word[0]
+    last_letter = word[-1]
+    middle_letters = list(word[1:-1])
+    random.shuffle(middle_letters)
+
+    return first_letter + ''.join(middle_letters) + last_letter
+
+
+
         
     
     
