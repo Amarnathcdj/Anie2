2c79a952f61be1d2991335a67ed89357d19a78fa
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-01-08 14:45:46 +0000

    Update utils.py
    B

--- userbot/utils.py
+++ userbot/utils.py
@@ -17,7 +17,120 @@ import subprocess
 import sys
 import traceback
 import datetime
+import asyncio
+import datetime
+import importlib
+import inspect
+import logging
+import math
+import re
+import sys
+import time
+import traceback
+from pathlib import Path
+from time import gmtime, strftime
 
+from telethon import events
+from telethon.tl.functions.channels import GetParticipantRequest
+from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
+
+from . import CMD_HELP, CMD_LIST, LOAD_PLUG, LOGS, SUDO_LIST, bot
+from .Config import Config
+from .helpers.exceptions import CancelProcess
+
+
+def load_module(shortname):
+    if shortname.startswith("__"):
+        pass
+    elif shortname.endswith("_"):
+        path = Path(f"userbot/plugins/{shortname}.py")
+        name = "userbot.plugins.{}".format(shortname)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(mod)
+        LOGS.info("Successfully imported " + shortname)
+    else:
+        import userbot.utils
+
+        from .helpers.tools import media_type
+        from .helpers.utils import install_pip, parse_pre, reply_id, run_async, run_sync
+        from .managers import edit_delete, edit_or_reply
+
+        path = Path(f"userbot/plugins/{shortname}.py")
+        name = "userbot.plugins.{}".format(shortname)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        mod.bot = bot
+        mod.Config = Config
+        mod.tgbot = bot.tgbot
+        mod.sudo_cmd = sudo_cmd
+        mod.CMD_HELP = CMD_HELP
+        mod.run_sync = run_sync
+        mod.reply_id = reply_id
+        mod.run_async = run_async
+        mod.admin_cmd = admin_cmd
+        mod.parse_pre = parse_pre
+        mod.media_type = media_type
+        mod.edit_delete = edit_delete
+        mod.install_pip = install_pip
+        mod.edit_or_reply = edit_or_reply
+        mod.logger = logging.getLogger(shortname)
+        # support for uniborg
+        sys.modules["uniborg.util"] = userbot.utils
+        mod.borg = bot
+        # support for paperplaneextended
+        sys.modules["userbot.events"] = userbot.utils
+        spec.loader.exec_module(mod)
+        # for imports
+        sys.modules["userbot.plugins." + shortname] = mod
+        LOGS.info("Successfully imported " + shortname)
+
+
+def remove_plugin(shortname):
+    try:
+        try:
+            for i in LOAD_PLUG[shortname]:
+                bot.remove_event_handler(i)
+            del LOAD_PLUG[shortname]
+
+        except BaseException:
+            name = f"userbot.plugins.{shortname}"
+
+            for i in reversed(range(len(bot._event_builders))):
+                ev, cb = bot._event_builders[i]
+                if cb.__module__ == name:
+                    del bot._event_builders[i]
+    except BaseException:
+        raise ValueError
+
+
+def admin_cmd(pattern=None, command=None, **args):
+    args["func"] = lambda e: e.via_bot_id is None
+    stack = inspect.stack()
+    previous_stack_frame = stack[1]
+    file_test = Path(previous_stack_frame.filename)
+    file_test = file_test.stem.replace(".py", "")
+    allow_sudo = args.get("allow_sudo", False)
+    # get the pattern from the decorator
+    if pattern is not None:
+        if pattern.startswith(r"\#"):
+            # special fix for snip.py
+            args["pattern"] = re.compile(pattern)
+        elif pattern.startswith(r"^"):
+            args["pattern"] = re.compile(pattern)
+            cmd = pattern.replace("$", "").replace("^", "").replace("\\", "")
+            try:
+                CMD_LIST[file_test].append(cmd)
+            except BaseException:
+                CMD_LIST.update({file_test: [cmd]})
+        else:
+            if len(Config.COMMAND_HAND_LER) == 2:
+                catreg = "^" + Config.COMMAND_HAND_LER
+                reg = Config.COMMAND_HAND_LER[1]
+            elif len(Config.COMMAND_HAND_LER) == 1:
+                catreg = "^\\" + Config.COMMAND_HAND_LER
+                reg = Config.COMMAND_HAND_LER
+            args["pattern"] = re.compile(ca
 from telethon.tl.functions.messages import GetPeerDialogsRequest
 
 from typing import List
